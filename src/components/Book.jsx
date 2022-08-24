cfddcb4cd047ff2e5963db3c4613e1928d9984bf
Mohamed Elrayes <mohamad_hesham1@hotmail.com>
2022-08-24 18:00:12 +0200

    img fix

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -3,7 +3,10 @@ import PropTypes from "prop-types";
 import BookDropDown from "./BookDropDown";
 
 function Book({ book, updateShelf }) {
-  const { id, title, authors, shelf } = book;
+  const { title, authors, imageLinks } = book;
+  let imgThumbnail = "";
+  imageLinks ? (imgThumbnail = imageLinks.thumbnail) : (imgThumbnail = "");
+
   return (
     <li>
       <div className="book">
@@ -13,14 +16,10 @@ function Book({ book, updateShelf }) {
             style={{
               width: 128,
               height: 193,
-              backgroundImage: `url("http://books.google.com/books/content?id=${id}&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")`,
+              backgroundImage: `url(${imgThumbnail})`,
             }}
           ></div>
-          <BookDropDown
-            bookShelf={shelf}
-            book={book}
-            updateShelf={updateShelf}
-          />
+          <BookDropDown book={book} updateShelf={updateShelf} />
         </div>
         <div className="book-title">{title}</div>
         <div className="book-authors">
